99be24dbfffbfac37341921cd38be2fd5da5342d
atlantic <dreamyouth@engineer.com>
2018-01-21 12:02:44 +0800

    git init

---
+++ CamVehicle/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CamVehicle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadVideoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadIPCamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(735, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadVideoToolStripMenuItem,
+            this.loadIPCamToolStripMenuItem,
+            this.loadImageToolStripMenuItem});
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // loadVideoToolStripMenuItem
+            // 
+            this.loadVideoToolStripMenuItem.Name = "loadVideoToolStripMenuItem";
+            this.loadVideoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadVideoToolStripMenuItem.Text = "Load Video";
+            this.loadVideoToolStripMenuItem.Click += new System.EventHandler(this.loadVideoToolStripMenuItem_Click);
+            // 
+            // loadIPCamToolStripMenuItem
+            // 
+            this.loadIPCamToolStripMenuItem.Name = "loadIPCamToolStripMenuItem";
+            this.loadIPCamToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadIPCamToolStripMenuItem.Text = "Load IP Cam";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 444);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadVideoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadIPCamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+    }
+}
+
---
+++ CamVehicle/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+using OpenCvSharp;
+using System.Diagnostics;
+
+namespace CamVehicle
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture cap;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadVideoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // create the video capture
+                string path = openFileDialog.FileName;
+                cap = VideoCapture.FromFile(path);
+
+                // display the input file name
+                string inFname = Path.GetFileName(path);
+                // lblInput.Text = "Video File: " + inFname;
+
+                int uFps = (int)cap.Get(CaptureProperty.Fps);
+                int uFrameWidth = (int)cap.Get(CaptureProperty.FrameWidth);
+                int uFrameHeight = (int)cap.Get(CaptureProperty.FrameHeight);
+                
+            }
+
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+                System.Diagnostics.Debug.WriteLine(path);
+                    
+                Mat matImg = Cv2.ImRead(path);
+                Cv2.ImShow("Image", matImg);
+                Cv2.WaitKey(0);
+            }
+        }
+    }
+}
